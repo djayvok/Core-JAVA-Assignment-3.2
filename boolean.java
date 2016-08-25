7e230c93bcc7c003d66f20297083cbefac178283
Dhananjaya <djay.vok@gmail.com>
2016-08-25 23:27:52 +0530

    Update and rename .gitignore to boolean.java

---
+++ boolean.java
@@ -0,0 +1,11 @@
+public class True {
+	public static void main(String[] args) {
+		boolean same = true;
+		String s1 = "False";
+		String s2 = s1;
+		System.out.println(same);
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
+
